b64b2de9bdebabdabd38ab8b9896bc646c528813
pandi <pandi@PandianSU.bbrouter>
2023-07-15 22:48:46 +0530

    GitApp

---
+++ ApplicationGit/src/main/java/com/example/demo/ApplicationGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGitApplication.class, args);
+	}
+
+}
